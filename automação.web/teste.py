99689e855e1476e7433f59366144c2ee842d5acd
Lucas Maciel <142741602+ShisuiElite@users.noreply.github.com>
2024-03-17 22:53:06 -0300

    Delete automação.web/teste.py

--- automação.web/teste.py
+++
@@ -1,7 +0,0 @@
-from openpyxl import load_workbook
-
-planilha_fornecedores = load_workbook('./fornecedores.xlsx')
-pagina_fornecedores = planilha_fornecedores['Sheet1']
-
-for linha in pagina_fornecedores.iter_rows( values_only= True):
-    print(linha)
